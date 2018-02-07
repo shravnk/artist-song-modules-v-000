4c443d58f88bddf45c79c9a1f4f61c458ec6ef4f
Brendan O'Dea <brendan.ode4@gmail.com>
2018-02-07 17:20:02 +0000

    automatically backed up by learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,10 +1,12 @@
 module Memorable
-  def reset_all
-    self.all.clear
-  end
+  module ClassMethods
+    def reset_all
+      self.all.clear
+    end
 
-  def count
-   self.all.count
+    def count
+     self.all.count
+    end
   end
 
   module InstanceMethods
